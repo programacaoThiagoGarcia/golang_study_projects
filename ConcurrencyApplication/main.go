89b8faa3a5025801332d5479dc97d3082cea0b75
ThiagoGarcia <thiago.garcia@itsector.pt>
2022-01-11 17:35:04 +0000

    Request com três métodos diferentes

--- ConcurrencyApplication/main.go
+++ ConcurrencyApplication/main.go
@@ -1,18 +1,73 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"sync"
 )
 
+//time go run main.go google.com itsector.pt github.com facebook.com twitter.com youtube.com
+
 func main() {
-	http.HandleFunc("/hello", getIndex)
+	args := os.Args[1:]
+	var wg sync.WaitGroup
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	if len(args) < 1 {
+		log.Fatalln("Use: go run main.go <url 1> <url 2> <url 3> ... <url n>")
+	}
 
+	makeRequest(args, &wg)
+	// makeRequestSync(args)
+	// makeRequestChan(args)
+
+	wg.Wait()
 }
 
-func getIndex(resp http.ResponseWriter, req *http.Request) {
-	log.Printf("%v", req.RequestURI)
+func makeRequest(urls []string, wg *sync.WaitGroup) {
+	var mutex sync.Mutex
+	for _, url := range urls {
+		wg.Add(1)
+		go func(url string) {
+			defer wg.Done()
+			resp, err := request(url)
+			if err != nil {
+				log.Fatal(err)
+			}
+			mutex.Lock()
+			defer mutex.Unlock()
+			fmt.Printf("[%v] %v \n", resp.StatusCode, url)
 
+		}(url)
+	}
+}
+
+func makeRequestSync(urls []string) {
+	for _, url := range urls {
+		resp, err := request(url)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("[%v] %v \n", resp.StatusCode, url)
+	}
+}
+
+func makeRequestChan(url []string) {
+	ch := make(chan int)
+	for _, url := range url {
+		go func(url string, ch chan<- int) {
+			resp, err := request(url)
+			if err != nil {
+				log.Fatal(err)
+			}
+			ch <- resp.StatusCode
+		}(url, ch)
+		fmt.Printf("[%v] %v \n", <-ch, url)
+
+	}
+}
+
+func request(url string) (resp *http.Response, err error) {
+	return http.Get("http://" + url)
 }
